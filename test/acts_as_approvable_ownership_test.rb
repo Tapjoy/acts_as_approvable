65a91d53eea9b26705887b874c0a78d35205e8a7
James Logsdon <dwarf@girsbrain.org>
2012-02-08 15:46:46 -0500

    Test that configuration of ownership works

---
+++ test/acts_as_approvable_ownership_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class ActsAsApprovableOwnershipTest < Test::Unit::TestCase
+  context 'with default configuration' do
+    setup { ActsAsApprovable::Ownership.configure }
+
+    should 'respond to .owner_class' do
+      assert_respond_to Approval, :owner_class
+    end
+
+    should 'respond to .available_owners' do
+      assert_respond_to Approval, :available_owners
+    end
+
+    should 'respond to .options_for_available_owners' do
+      assert_respond_to Approval, :options_for_available_owners
+    end
+
+    should 'respond to .assigned_owners' do
+      assert_respond_to Approval, :assigned_owners
+    end
+
+    should 'respond to .options_for_assigned_owners' do
+      assert_respond_to Approval, :options_for_assigned_owners
+    end
+  end
+
+  context 'given a block' do
+    setup do
+      ActsAsApprovable::Ownership.configure do
+        def self.available_owners
+          [1, 2, 3]
+        end
+      end
+    end
+
+    should 'allow overriding methods' do
+      assert_equal [1, 2, 3], Approval.available_owners
+    end
+  end
+end
