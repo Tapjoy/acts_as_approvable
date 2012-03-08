dabfe62973be917baebd43468d5391b02bbac867
James Logsdon <dwarf@girsbrain.org>
2012-03-08 15:05:27 -0500

    extend matcher checks both extend and include now

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -1,8 +1,17 @@
 require 'rspec/expectations'
 
+class Module
+  # Return any modules we +extend+
+  def extended_modules
+    (class << self; self end).included_modules
+  end
+end
+
+# Truthfully this checks both extend *and* include. :include is already used as a matcher for Arrays :-/
 RSpec::Matchers.define :extend do |expected|
   match do |actual|
-    actual.included_modules.include?(expected)
+    extended = (actual.extended_modules - Module.extended_modules) + actual.included_modules
+    extended.include?(expected)
   end
   description do
     "extend #{expected}"
