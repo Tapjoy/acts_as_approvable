02d6f0b8c763119960e548149ca682ced1da6950
James Logsdon <dwarf@girsbrain.org>
2012-03-09 17:40:18 -0500

    Remove Test::Unit cases

--- test/acts_as_approvable_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class ActsAsApprovableTest < Test::Unit::TestCase
-  should 'set VERSION contanst to file contents' do
-    contents = File.read(File.expand_path(File.join(File.dirname(__FILE__), '..', 'VERSION'))).chomp
-    assert_equal ActsAsApprovable::VERSION, contents
-  end
-end
